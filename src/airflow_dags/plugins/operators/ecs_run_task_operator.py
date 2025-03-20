cc90e92b399d2872428e5e546309cec938e778eb
devsjc <47188100+devsjc@users.noreply.github.com>
2025-03-20 14:44:24 +0000

    fix(operator): Add region

--- src/airflow_dags/plugins/operators/ecs_run_task_operator.py
+++ src/airflow_dags/plugins/operators/ecs_run_task_operator.py
@@ -159,7 +159,7 @@ class ECSOperatorGen:
         )
 
         try:
-            client = boto3.client("ecs")
+            client = boto3.client("ecs", region=region)
             existing_def = client.describe_task_definition(
                 taskDefinition=self.name, include=["TAGS"],
             )
