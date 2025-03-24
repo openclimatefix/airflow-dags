e1dc4c0e293c6095687c77d97ef09b99c850db38
devsjc <47188100+devsjc@users.noreply.github.com>
2025-03-24 11:58:26 +0000

    fix(nwp): Add latestonly import

--- src/airflow_dags/dags/uk/consume-nwp-dag.py
+++ src/airflow_dags/dags/uk/consume-nwp-dag.py
@@ -6,6 +6,7 @@ import os
 
 from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
+from airflow.operators.latest_only import LatestOnlyOperator
 
 from airflow_dags.plugins.callbacks.slack import slack_message_callback
 from airflow_dags.plugins.operators.ecs_run_task_operator import (
