fd5cc569451734535e39e73b7347eec2c3db9568
Sukhil Patel <sukhil@Sukhils-MacBook-Pro.local>
2025-04-24 10:54:58 +0100

    Update message

--- src/airflow_dags/dags/uk/consume-sat-dag.py
+++ src/airflow_dags/dags/uk/consume-sat-dag.py
@@ -108,7 +108,7 @@ def sat_consumer_dag() -> None:
             "which doesn't need satellite data. "
             "EUMETSAT status link for the RSS service (5 min) is "
             "<https://masif.eumetsat.int/ossi/webpages/level3.html?ossi_level3_filename"
-            "=seviri_rss_hr.json.html&ossi_level2_filename=seviri_rss.html|here>. "
+            "=seviri_rss_hr.json.html&ossi_level2_filename=seviri_rss.html|here> "
             "and the 0 degree (15 minute) which we use as a backup is "
             "<https://masif.eumetsat.int/ossi/webpages/level3.html?ossi_level3_filename"
             "=seviri_0deg_hr.json.html&ossi_level2_filename=seviri_0deg.html|here>. "
