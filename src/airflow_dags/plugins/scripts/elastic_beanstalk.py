51beccb884b4a6635b75cdf2e0ef64ccfdce4334
peterdudfield <peter.dudfield@hotmail.com>
2025-07-14 16:47:26 +0100

    terminate old ec2 instances

--- src/airflow_dags/plugins/scripts/elastic_beanstalk.py
+++ src/airflow_dags/plugins/scripts/elastic_beanstalk.py
@@ -1,11 +1,42 @@
 """Functions for elastic beanstalk environment."""
-
+from datetime import datetime, timezone
 import logging
 import time
 
 import boto3
 
 logger = logging.getLogger(__name__)
+
+
+def terminate_any_old_instances(name: str, days_limit: int = 3, sleep_seconds: int = 300):
+    """Terminate any old instances in an elastic beanstalk environment.
+
+    Note when an instance in terminate in ELB, the ELB will automatically start a new one up
+    Args:
+        name: Name of the elastic beanstalk environment.
+        days_limit: Number of days to consider an instance as old.
+        sleep_seconds: Time to wait after terminating each instance.
+    """
+    eb = boto3.client("elasticbeanstalk")
+    ec2 = boto3.client("ec2")
+    response = eb.describe_environment_resources(EnvironmentName=name)
+
+    number_of_instances = len(response['EnvironmentResources']['Instances'])
+    if number_of_instances < 2:
+        logger.info(f"Only {number_of_instances} instance(s) found in environment '{name}'. "
+                    f"Termination would be too risky.")
+        return
+
+    for instance in response['EnvironmentResources']['Instances']:
+        instance_details = ec2.describe_instances(InstanceIds=[instance['Id']])
+        launch_datetime = instance_details['Reservations'][0]['Instances'][0]['LaunchTime']
+        if (datetime.now(tz=timezone.utc) - launch_datetime).days > days_limit:
+            logger.info(f"Terminating old instance {instance['Id']} launched at {launch_datetime}")
+            ec2 = boto3.client('ec2')
+            ec2.terminate_instances(InstanceIds=[instance['Id']])
+            time.sleep(sleep_seconds)
+        else:
+            logger.info(f"Instance {instance['Id']} is not old enough, skipping termination.")
 
 
 def scale_elastic_beanstalk_instance(
