3cdc759a56a6eab8ebe322b4a659f74616e69969
peterdudfield <peter.dudfield@hotmail.com>
2025-08-04 14:04:48 +0100

    fix for saving to zarr.zip

--- src/airflow_dags/plugins/scripts/s3.py
+++ src/airflow_dags/plugins/scripts/s3.py
@@ -1,10 +1,10 @@
 """Helper functions that handle S3 files."""
 
-import shutil
 import tempfile
 
 import icechunk
 import xarray as xr
+import zarr
 from airflow.decorators import task
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
@@ -80,23 +80,17 @@ def extract_latest_zarr(bucket: str, prefix: str, window_mins: int, cadence_mins
     store_ds = xr.open_zarr(store=session.store, consolidated=False)
     desired_image_num: int = window_mins // cadence_mins
     s3hook.log.info(f"Extracting latest zarr from {prefix} with window of {window_mins} minutes")
-    with tempfile.TemporaryDirectory(suffix=".zarr") as tmpzarr:
-        store_ds.isel(time=slice(-desired_image_num, None)).to_zarr(
-            tmpzarr,
-            mode="w",
-            consolidated=True,
-        )
-        # Zip the zarr directory
-        shutil.make_archive(
-            base_name=tmpzarr + "/latest",
-            root_dir=tmpzarr,
-            base_dir=".",
-            format="zip",
-        )
-        s3hook.load_file(
-            filename=f"{tmpzarr}/latest.zip",
-            bucket_name=bucket,
-            replace=True,
-            key=f"{prefix.rsplit('/', 1)[0]}/latest.zarr.zip",
-        )
+    dataset = store_ds.isel(time=slice(-desired_image_num, None))
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # make zarr.zip
+        path = f"{tmpdir}/temp.zarr.zip"
+        with zarr.ZipStore(path) as store:
+            dataset.to_zarr(store, compute=True, mode="w", consolidated=True)
+
+            s3hook.load_file(
+                filename=f"{tmpdir}/latest.zarr.zip",
+                bucket_name=bucket,
+                replace=True,
+                key=f"{prefix.rsplit('/', 1)[0]}/latest.zarr.zip",
+            )
     s3hook.log.info(f"Extracted latest zarr to {bucket}/{prefix}/latest.zarr.zip")
