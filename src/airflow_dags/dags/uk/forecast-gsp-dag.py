2458448584bbf6b0e6ae13a5355d835a38362bd9
Peter Dudfield <peter.dudfield@hotmail.com>
2025-08-12 14:19:07 +0100

    add SATELLITE_15_ZARR_PATH to gsp forecast

--- src/airflow_dags/dags/uk/forecast-gsp-dag.py
+++ src/airflow_dags/dags/uk/forecast-gsp-dag.py
@@ -41,6 +41,7 @@ gsp_forecaster_args = dict(  # noqa: C408
         "NWP_ECMWF_ZARR_PATH": f"s3://nowcasting-nwp-{env}/ecmwf/data/latest.zarr",
         "NWP_UKV_ZARR_PATH": f"s3://nowcasting-nwp-{env}/data-metoffice/latest.zarr",
         "SATELLITE_ZARR_PATH": f"s3://nowcasting-sat-{env}/rss/data/latest.zarr.zip",
+        "SATELLITE_15_ZARR_PATH": f"s3://nowcasting-sat-{env}/odegree/data/latest.zarr.zip",
         "CLOUDCASTING_ZARR_PATH": f"s3://nowcasting-sat-{env}/cloudcasting_forecast/latest.zarr",
         "SATELLITE_SCALE_FACTOR": "1",
     },
