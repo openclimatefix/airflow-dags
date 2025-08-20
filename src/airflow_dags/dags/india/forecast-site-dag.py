1fe5ee875e46943eeb14bea46f35e1c56c441cf0
Peter Dudfield <peter.dudfield@hotmail.com>
2025-08-20 14:16:05 +0100

    fix typo

--- src/airflow_dags/dags/india/forecast-site-dag.py
+++ src/airflow_dags/dags/india/forecast-site-dag.py
@@ -50,7 +50,7 @@ ad_forecaster = ContainerDefinition(
     container_env={
         "NWP_MO_GLOBAL_ZARR_PATH": f"s3://india-nwp-{env}/metoffice/data/latest.zarr",
         "NWP_ECMWF_ZARR_PATH": f"s3://india-nwp-{env}/ecmwf/data/latest.zarr",
-        "SATELLITE_ZARR_PATH": f"s3://india-satellite-{env}/idoc/data/latest.zarr.zip",
+        "SATELLITE_ZARR_PATH": f"s3://india-satellite-{env}/iodc/data/latest.zarr.zip",
         "CLIENT_NAME": "ad",
         "COUNTRY": "india",
     },
