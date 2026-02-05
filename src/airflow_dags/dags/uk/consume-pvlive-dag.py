908d1827d617b02456509a8cc9c2a8f0ab1eea22
devsjc <47188100+devsjc@users.noreply.github.com>
2026-02-05 17:01:57 +0000

    Add release notes for version 1.33 (#489)
    #minor

--- src/airflow_dags/dags/uk/consume-pvlive-dag.py
+++ src/airflow_dags/dags/uk/consume-pvlive-dag.py
@@ -84,7 +84,7 @@ def pvlive_intraday_consumer_dag() -> None:
         airflow_task_id="pvlive-intraday-consumer-gsps",
         container_def=pvlive_consumer,
         env_overrides={
-            "N_GSPS": "348",
+            "N_GSPS": "342",
             "REGIME": "in-day",
             "BACKFILL_HOURS": "12",
         },
@@ -142,7 +142,7 @@ def pvlive_dayafter_consumer_dag() -> None:
         airflow_task_id="consume-pvlive-dayafter-gsps",
         container_def=pvlive_consumer,
         env_overrides={
-            "N_GSPS": "348",
+            "N_GSPS": "342",
             "REGIME": "day-after",
         },
         on_failure_callback=get_slack_message_callback(
