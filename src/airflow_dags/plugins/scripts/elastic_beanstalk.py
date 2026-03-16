e452a21bd4fb7669093f0064894c0a6b657a1549
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2026-03-16 14:06:25 +0100

    kill oldest ec2 instances (#525)
    * kill oldest ec2 instances
    
    * lint
    
    * lint
    
    * update logging
    
    * lint

--- src/airflow_dags/plugins/scripts/elastic_beanstalk.py
+++ src/airflow_dags/plugins/scripts/elastic_beanstalk.py
@@ -2,20 +2,18 @@
 
 import logging
 import time
-from datetime import UTC, datetime
 
 import boto3
 
 logger = logging.getLogger(__name__)
 
 
-def terminate_any_old_instances(name: str, days_limit: int = 2, sleep_seconds: int = 300) -> None:
+def terminate_any_old_instances(name: str, sleep_seconds: int = 300) -> None:
     """Terminate any old instances in an elastic beanstalk environment.
 
     Note when an instance in terminate in ELB, the ELB will automatically start a new one up
     Args:
         name: Name of the elastic beanstalk environment.
-        days_limit: Number of days to consider an instance as old.
         sleep_seconds: Time to wait after terminating each instance.
     """
     eb = boto3.client("elasticbeanstalk")
@@ -30,10 +28,22 @@ def terminate_any_old_instances(name: str, days_limit: int = 2, sleep_seconds: i
         )
         return
 
+    # get all launch datetimes
+    launch_datetimes = {}
     for instance in response["EnvironmentResources"]["Instances"]:
         instance_details = ec2.describe_instances(InstanceIds=[instance["Id"]])
         launch_datetime = instance_details["Reservations"][0]["Instances"][0]["LaunchTime"]
-        if (datetime.now(tz=UTC) - launch_datetime).days > days_limit:
+        launch_datetimes[instance["Id"]] = launch_datetime
+
+    # Find the youngest instance
+    youngest_instance_id = max(launch_datetimes, key=lambda k: launch_datetimes[k])
+    logger.info(f"Youngest instance is {youngest_instance_id} "
+                f"launched at {launch_datetimes[youngest_instance_id]}")
+
+    for instance in response["EnvironmentResources"]["Instances"]:
+        instance_details = ec2.describe_instances(InstanceIds=[instance["Id"]])
+        launch_datetime = instance_details["Reservations"][0]["Instances"][0]["LaunchTime"]
+        if instance["Id"] != youngest_instance_id:
             logger.info(f"Terminating old instance {instance['Id']} launched at {launch_datetime}")
             ec2 = boto3.client("ec2")
             ec2.terminate_instances(InstanceIds=[instance["Id"]])
@@ -43,7 +53,8 @@ def terminate_any_old_instances(name: str, days_limit: int = 2, sleep_seconds: i
             )
             time.sleep(sleep_seconds)
         else:
-            logger.info(f"Instance {instance['Id']} is not old enough, skipping termination.")
+            logger.info(f"Instance {instance['Id']} is the youngest, skipping termination.")
+
 
 
 def scale_elastic_beanstalk_instance(
