17fdb036c38748c9b62462c8661de79788da32e8
Peter Dudfield <peter.dudfield@hotmail.com>
2026-03-16 11:21:57 +0000

    kill oldest ec2 instances

--- src/airflow_dags/plugins/scripts/elastic_beanstalk.py
+++ src/airflow_dags/plugins/scripts/elastic_beanstalk.py
@@ -30,10 +30,21 @@ def terminate_any_old_instances(name: str, days_limit: int = 2, sleep_seconds: i
         )
         return
 
+    # get all launch datetimes
+    launch_datetimes = {}
     for instance in response["EnvironmentResources"]["Instances"]:
         instance_details = ec2.describe_instances(InstanceIds=[instance["Id"]])
         launch_datetime = instance_details["Reservations"][0]["Instances"][0]["LaunchTime"]
-        if (datetime.now(tz=UTC) - launch_datetime).days > days_limit:
+        launch_datetimes[instance["Id"]] = launch_datetime
+
+    # Find the youngest instance
+    youngest_instance_id = max(launch_datetimes, key=launch_datetimes.get)
+    logger.info(f"Youngest instance is {youngest_instance_id} launched at {launch_datetimes[youngest_instance_id]}")
+
+    for instance in response["EnvironmentResources"]["Instances"]:
+        instance_details = ec2.describe_instances(InstanceIds=[instance["Id"]])
+        launch_datetime = instance_details["Reservations"][0]["Instances"][0]["LaunchTime"]
+        if instance["Id"] != youngest_instance_id:
             logger.info(f"Terminating old instance {instance['Id']} launched at {launch_datetime}")
             ec2 = boto3.client("ec2")
             ec2.terminate_instances(InstanceIds=[instance["Id"]])
